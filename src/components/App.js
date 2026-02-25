783f1db0b44924b998808740f45a420eba508cb1
Artur Czerwiak <arturczerwiak@MacBook-Air-Artur.local>
2026-02-25 19:40:09 +0100

    Task 02 completed

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,30 @@ import Task02 from './../../02/Task02';
 import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 const App = () => {
-    return (
-        <>
-            <Task01 />
-            {/* <Task02 /> */}
-            {/* <Task03 /> */}
-            {/* <Task04 /> */}
-            {/* <Task05 /> */}
-        </>
-    )
-}
+	const initState = { message: 'Działa!' };
+	const reducer = (state = initState, action) => {
+		switch (action.type) {
+			default:
+				return state;
+		}
+	};
+	const store = createStore(reducer);
+
+	return (
+		<>
+			{/* <Task01 /> */}
+			<Provider store={store}>
+				<Task02 />
+			</Provider>
+			{/* <Task03 /> */}
+			{/* <Task04 /> */}
+			{/* <Task05 /> */}
+		</>
+	);
+};
 
 export default App;
